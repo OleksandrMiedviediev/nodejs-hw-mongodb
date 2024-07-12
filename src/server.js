2a13c0c4e6e69896732071ecd13a2ef4bb63e012
Olek Miedviediev <medvedevbasket@gmail.com>
2024-07-12 08:47:26 +0200

    add

---
+++ src/server.js
@@ -0,0 +1,38 @@
+import express, { request, response } from "express";
+import contacts from "../contacts.js";
+import cors from "cors"
+import dotenv from "dotenv";
+import pino from "pino-http";
+
+
+dotenv.config();
+const PORT = Number(process.env.PORT);
+
+const setupServer = () => {
+    const app = express();
+
+    const logger = pino({
+        transport: {
+            target: "pino-pretty"
+        }
+    })
+
+    app.use(logger)
+    app.use(cors());
+
+    app.get("/api", (request, response) => {
+        response.send('<h1>Home page</h1>');
+    })
+    app.get("/api/contacts", (request, response) => {
+        response.json(contacts);
+    })
+
+    app.use((req, res) => {
+        res.status(404).json({
+            message: "Not Found"
+        })
+    })
+    app.listen(3000, () => console.log(`Server is running on port ${PORT}`));
+}
+
+export default setupServer;
