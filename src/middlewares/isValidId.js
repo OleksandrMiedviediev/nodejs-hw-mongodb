ba1b21f975dfa47d132870fa63f6661791e0454e
Olek Miedviediev <medvedevbasket@gmail.com>
2024-07-14 04:08:30 +0200

    add

--- src/middlewares/isValidId.js
+++ src/middlewares/isValidId.js
@@ -5,7 +5,7 @@ import createHttpError from "http-errors";
 const isValidId = (req, res, next) => {
     const { contactId } = req.params;
     if (!isValidObjectId(contactId)) {
-        return next(createHttpError(404, "Contact not found"))
+        return next(createHttpError(404, `${contactId} not valid id`))
     }
     next();
 }
